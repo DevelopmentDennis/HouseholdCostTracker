2dd81a554d9828cd5ca687e321f7f7c9517223c6
Dennis <development@dennisostertag.de>
2020-12-22 23:38:59 +0100

    fixed import

--- src/components/DateTimeInput/index.tsx
+++ src/components/DateTimeInput/index.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import {Component} from 'react';
+import * as React, {Component} from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import {Input} from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
